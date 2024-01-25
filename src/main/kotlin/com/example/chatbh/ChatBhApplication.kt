fafd3b320e06575e0b8006577e4b2ade5885dd72
hanbonghun <bng4535@gmail.com>
2024-01-25 18:35:09 +0900

    chore: 프로젝트 생성

---
+++ src/main/kotlin/com/example/chatbh/ChatBhApplication.kt
@@ -0,0 +1,11 @@
+package com.example.chatbh
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChatBhApplication
+
+fun main(args: Array<String>) {
+    runApplication<ChatBhApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/chatbh/ChatBhApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.chatbh
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChatBhApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
