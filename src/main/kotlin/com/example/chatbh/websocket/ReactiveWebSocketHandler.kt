3b222776e38a6b5dcab6eba9962e3f20fc104545
hanbonghun <bng4535@gmail.com>
2024-01-30 01:28:43 +0900

    feat: 핸들러 적용

--- src/main/kotlin/com/example/chatbh/websocket/ReactiveWebSocketHandler.kt
+++ src/main/kotlin/com/example/chatbh/websocket/ReactiveWebSocketHandler.kt
@@ -1,83 +1,134 @@
 package com.example.chatbh.websocket
 
+import com.example.chatbh.ChatRoomChannelService
+import com.example.chatbh.chatroom.service.ChatRoomService
+import com.example.chatbh.chatroom.service.ChatRoomSessionService
+import com.example.chatbh.user.service.UserSessionService
 import com.example.chatbh.websocket.service.WebSocketSessionManager
+import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.socket.WebSocketHandler
 import org.springframework.web.reactive.socket.WebSocketSession
-import org.springframework.web.util.UriComponentsBuilder
-import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 @Component
 class ReactiveWebSocketHandler(
     private val sessionManager: WebSocketSessionManager,
+    private val chatRoomService: ChatRoomService,
+    private val chatRoomChannelService: ChatRoomChannelService,
+    private val userSessionService: UserSessionService,
+    private val chatRoomSessionService: ChatRoomSessionService,
 ) : WebSocketHandler {
     private val objectMapper = jacksonObjectMapper()
 
     override fun handle(session: WebSocketSession): Mono<Void> {
-        val username = getUsernameFromSession(session)
-        println("Connected user: $username")
+        val userId = sessionManager.getUserIdFromSession(session)
+        println("Connected userId: $userId")
 
         // 세션 등록
-        username?.let { sessionManager.registerSession(session.id, it, session) }
+        userId?.let {
+            sessionManager.registerSession(session.id, it, session)
+            userSessionService.mapSessionToUser(session.id, userId)
+        }
 
         val outputMessages = session.receive()
             .map { msg -> handleMessage(msg.payloadAsText, session) }
             .then()
 
         return outputMessages.doFinally {
-            // 세션 해제
+            println("세션 해제 ")
             sessionManager.removeSession(session.id)
         }
     }
 
-    private fun getUsernameFromSession(session: WebSocketSession): String? {
-        val uri = session.handshakeInfo.uri.toString()
-        val uriComponents = UriComponentsBuilder.fromUriString(uri).build()
-        return uriComponents.queryParams.getFirst("username")
-    }
-
     private fun handleMessage(messageText: String, session: WebSocketSession): Mono<Void> {
         return try {
-            val message = objectMapper.readValue(messageText, ChatMessage::class.java)
-            when (message.command) {
-                "join", "create" -> handleJoinOrCreateChatRoom(message, session)
-                "send" -> broadcastMessage(message)
+            val request = objectMapper.readValue(messageText, WebSocketRequest::class.java)
+            when (request.type) {
+                "create" -> {
+                    val payload =
+                        objectMapper.treeToValue(request.payload, ChatRoomCreatePayload::class.java)
+                    handleCreateChatRoom(payload, session)
+                }
+                "join" -> {
+                    val payload =
+                        objectMapper.treeToValue(request.payload, ChatRoomJoinPayload::class.java)
+                    handleJoinChatRoom(payload, session)
+                }
+                "send" -> {
+                    val payload = objectMapper.treeToValue(request.payload, ChatRoomMessagePayload::class.java)
+                    handleSendMessage(payload, session)
+                }
                 else -> Mono.empty()
             }
         } catch (e: Exception) {
-            println("Error parsing message: $messageText")
-            Mono.empty<Void>()
+            println("Error parsing message: $messageText, Exception: ${e.message}")
+            e.printStackTrace()
+            Mono.error(e)
         }
     }
 
-    private fun handleJoinOrCreateChatRoom(message: ChatMessage, session: WebSocketSession): Mono<Void> {
-        sessionManager.addUserToChatRoom(message.sender, message.chatRoomId)
-        return Mono.empty<Void>()
+    private fun handleCreateChatRoom(
+        payload: ChatRoomCreatePayload,
+        session: WebSocketSession
+    ): Mono<Void> {
+        chatRoomService.createChatRoom(payload.name, payload.userIds.toSet())
+            .flatMap { chatRoom ->
+                chatRoomChannelService.createChatRoomChannel(chatRoom.id)
+            }
+            .onErrorResume { e ->
+                println("채팅방 생성 중 에러 발생: ${e.message}")
+                Mono.empty()
+            }
+            .subscribe()
+
+        return Mono.empty()
+
     }
 
-    // TODO: blocking 되는 부분 수정 필요
-    private fun broadcastMessage(message: ChatMessage): Mono<Void> {
-        val sessions = sessionManager.getSessionsForChatRoom(message.chatRoomId)
-        val broadcast = Flux.fromIterable(sessions)
-            .flatMap { sessionToSend ->
-                if (sessionToSend != null) {
-                    sessionToSend.send(Mono.just(sessionToSend.textMessage(objectMapper.writeValueAsString(message))))
-                } else {
-                    println("메세지를 받을 세션이 존재하지 않습니다.")
-                    Mono.empty<Void>()
-                }
-            }.subscribe()
+    private fun handleJoinChatRoom(
+        payload: ChatRoomJoinPayload,
+        session: WebSocketSession
+    ): Mono<Void> {
+        val chatRoomId = payload.chatRoomId
+        val userId = payload.userId
+
+        // 세션을 채팅방에 매핑
+        chatRoomSessionService.addSessionToChatRoom(session.id, chatRoomId)
+        userSessionService.mapSessionToUser(session.id, userId)
+
+        return Mono.empty()
+    }
+
+    private fun handleSendMessage(
+        payload: ChatRoomMessagePayload,
+        session: WebSocketSession
+    ): Mono<Void> {
+        val messageJson = objectMapper.writeValueAsString(payload)
+        chatRoomChannelService.publishToChatRoomChannel(payload.chatRoomId, messageJson)
 
         return Mono.empty()
     }
 }
 
-data class ChatMessage(
-    val command: String,
-    val content: String,
-    val sender: String,
+data class WebSocketRequest(
+    val type: String,
+    val payload: JsonNode
+)
+
+data class ChatRoomCreatePayload(
+    val name: String,
+    val userIds: Set<String>
+)
+
+data class ChatRoomJoinPayload(
     val chatRoomId: String,
-    val participants: Set<String>,
+    val userId: String
+)
+
+data class ChatRoomMessagePayload(
+    val chatRoomId: String,
+    val senderId: String,
+    val message: String
 )
