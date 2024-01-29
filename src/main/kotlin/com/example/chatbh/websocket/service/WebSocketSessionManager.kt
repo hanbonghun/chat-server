efa3a7db2308becd9c5832d69a386179b3d85d14
hanbonghun <bng4535@gmail.com>
2024-01-30 01:28:34 +0900

    feat: 웹소켓 세션 매니저 구현

--- src/main/kotlin/com/example/chatbh/websocket/service/WebSocketSessionManager.kt
+++ src/main/kotlin/com/example/chatbh/websocket/service/WebSocketSessionManager.kt
@@ -2,36 +2,29 @@ package com.example.chatbh.websocket.service
 
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.socket.WebSocketSession
+import org.springframework.web.util.UriComponentsBuilder
 import java.util.concurrent.ConcurrentHashMap
 
 
 @Service
 class WebSocketSessionManager {
     private val sessions: MutableMap<String, WebSocketSession> = ConcurrentHashMap()
-    private val userSessions: MutableMap<String, String> = ConcurrentHashMap()
-    private val chatRoomSessions: MutableMap<String, MutableSet<String>> = ConcurrentHashMap()
 
     fun registerSession(sessionId: String, username: String, session: WebSocketSession) {
         sessions[sessionId] = session
-        userSessions[username] = sessionId
     }
 
     fun removeSession(sessionId: String) {
         sessions.remove(sessionId)
-        userSessions.values.remove(sessionId)
-        chatRoomSessions.values.forEach { it.remove(sessionId) }
-    }
-
-    fun addUserToChatRoom(username: String, chatRoomId: String) {
-        val sessionId = userSessions[username] ?: return
-        chatRoomSessions.computeIfAbsent(chatRoomId) { ConcurrentHashMap.newKeySet() }.add(sessionId)
-    }
-
-    fun getSessionsForChatRoom(chatRoomId: String): Set<WebSocketSession> {
-        return chatRoomSessions[chatRoomId]?.mapNotNull { sessions[it] }?.toSet() ?: emptySet()
     }
 
     fun getSessionById(sessionId: String): WebSocketSession? {
         return sessions[sessionId]
+    }
+
+    fun getUserIdFromSession(session: WebSocketSession): String? {
+        val uri = session.handshakeInfo.uri.toString()
+        val uriComponents = UriComponentsBuilder.fromUriString(uri).build()
+        return uriComponents.queryParams.getFirst("userId")
     }
 }
