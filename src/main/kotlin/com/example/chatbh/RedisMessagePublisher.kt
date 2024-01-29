1ef0d8cbfe57cf6eae2a1df687fa35d93724a40c
hanbonghun <bng4535@gmail.com>
2024-01-30 01:28:49 +0900

    chore: 임시 주석

--- src/main/kotlin/com/example/chatbh/RedisMessagePublisher.kt
+++ src/main/kotlin/com/example/chatbh/RedisMessagePublisher.kt
@@ -3,11 +3,11 @@ package com.example.chatbh
 import org.springframework.data.redis.core.ReactiveRedisTemplate
 import org.springframework.stereotype.Service
 
-@Service
-class RedisMessagePublisher(private val reactiveRedisTemplate: ReactiveRedisTemplate<String, String>) {
-
-    fun publishToChatRoom(chatRoomId: String, message: String) {
-        val chatRoomTopic = "chatRoom:$chatRoomId"
-        reactiveRedisTemplate.convertAndSend(chatRoomTopic, message).subscribe()
-    }
-}
+//@Service
+//class RedisMessagePublisher(private val reactiveRedisTemplate: ReactiveRedisTemplate<String, String>) {
+//
+//    fun publishToChatRoom(chatRoomId: String, message: String) {
+//        val chatRoomTopic = "chatRoom:$chatRoomId"
+//        reactiveRedisTemplate.convertAndSend(chatRoomTopic, message).subscribe()
+//    }
+//}
